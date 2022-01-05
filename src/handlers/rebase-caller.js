a372ee5153a7c666e05612de9567c8f08ad81062
Andrew English <aje299@gmail.com>
2022-01-05 15:48:19 -0800

    Add functionality to call rebase on event

--- src/handlers/rebase-caller.js
+++ src/handlers/rebase-caller.js
@@ -1,3 +1,44 @@
+const { connect } = require('../../lib/web3-connector');
+const { seedHex, generateNewSeedHash } = require('../../lib/utils');
+const RBPoolControllerABI = require('../../lib/contracts/RBPoolController.json').abi;
+
 exports.rebaseCallerHandler = async (event, context) => {
-	console.info(JSON.stringify(event));
+  const { web3, deployer_address, deployer_key } = await connect(event.env);
+  const { controllerAddress: contractAddress } = JSON.parse(event.MessageBody)
+	const contract = new web3.eth.Contract(RBPoolControllerABI, contractAddress);
+
+  if(!contractAddress || !deployer_address || !deployer_key || !web3 || !contract) {
+    console.error('Missing info to continue');
+    return;
+  }
+  
+  // fetch seedKey
+  const seedKey = seedHex(web3, 'something');
+  
+  // generate new seedKey and hash
+  const { newSeedKey, newSeedHash } = generateNewSeedHash(web3, deployer_address);
+
+  let tx = {
+    from: deployer_address,
+    to: contractAddress,
+    nonce: await web3.eth.getTransactionCount(deployer_address, "latest"), // get latest nonce,
+    data: contract.methods.rebase(seedKey, newSeedHash).encodeABI()
+  }
+
+  try {
+    tx['gas'] = `0x${(await web3.eth.estimateGas(tx)).toString(16)}`
+  } catch (e) {
+    console.info('Error estimating gas');
+    console.error(e);
+    return;
+  }
+
+  const signedTx = await web3.eth.accounts.signTransaction(tx, deployer_key);
+
+  try {
+    const prom = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+    console.info('tx hash:', prom.transactionHash);
+  } catch (e) {
+    console.error(e);
+  }
 }
